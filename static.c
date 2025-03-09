28cadfd097481d13a198a205c0f30e278d8fed79
Huzaifakhalid1 <huzaifa.p.12.2@gmail.com>
2025-03-09 20:28:36 +0500

    3rd commit 4th commit

---
+++ static.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define N 300  
+
+int main() {
+    static int A[N][N], B[N][N], C[N][N];
+
+    // Initialize matrices
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = i + j;
+            B[i][j] = i - j;
+            C[i][j] = 0;
+        }
+    }
+
+    double start = omp_get_wtime();
+
+    #pragma omp parallel for collapse(2) schedule(static)
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            int sum = 0;
+            for (int k = 0; k < N; k++) {
+                sum += A[i][k] * B[k][j];
+            }
+            C[i][j] = sum;
+        }
+    }
+
+    double end = omp_get_wtime();
+
+    printf("Static Scheduling Execution Time: %f seconds\n", end - start);
+
+    return 0;
+}
